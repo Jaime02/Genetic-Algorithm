8eb967fc2c88628d76c5b8a31d3d23fbec6c5717
Jaime Resano <resano.138736@e.unavarra.es>
2023-05-27 23:41:21 +0200

    Add dark theme

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import qdarktheme
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication
 
@@ -8,10 +9,11 @@ from result import Result
 def main():
     results = Result.read_all_results()
     app = QApplication()
-    app.setStyle("Fusion")
     app.setApplicationName("Genetic Algorithm")
     app.setApplicationDisplayName("Genetic Algorithm")
     app.setWindowIcon(QPixmap("icon.png"))
+    qdarktheme.setup_theme()
+
     window = MainWindow()
     window.add_results(results)
     window.show()
